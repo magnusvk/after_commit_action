ff0573bf1b2dfe0ee7dd27965c9682b4e5925a6c
Magnus von Koeller <magnus@vonkoeller.de>
2012-05-23 17:26:32 -0400

    moved file from bestvendor_code

---
+++ lib/after_commit_action.rb
@@ -0,0 +1,55 @@
+# use this module to defer actions to the after-commit hook. this is useful if you want
+# to trigger actions in after_create, after_destroy and after_update callbacks but want
+# to execute them outside of the transaction (for example, to avoid deadlocks).
+# 
+# Usage:
+# after_create :my_hook
+# def my_hook
+#   execute_after_commit { puts "This is called after committing the transaction. "}
+# end
+
+module AfterCommitAction
+  
+  module ActiveRecord
+
+    def self.included(base)
+      # also add class methods to ActiveRecord::Base
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+
+      def self.included(base)
+        base.after_commit :_after_commit_hook
+      end
+
+    end
+
+    def execute_after_commit(&block)
+      @_execute_after_commit ||= []
+      @_execute_after_commit<< block
+    end
+    
+    def _after_commit_hook
+      begin
+        until @_execute_after_commit.blank?
+          @_execute_after_commit.shift.call
+        end
+      rescue => e
+        if defined?(Exceptional)
+          # Rails quietly swallows exceptions in after-commit actions; to avoid missing these
+          # exceptions, we pass them to Exceptional explicitly
+          Exceptional.context(:after_commit_entity => self.inspect)
+          Exceptional.handle(e, "execute_after_commit")
+        else
+          Rails.logger.error "Error in execute_after_commit block: #{e.inspect}"
+        end
+        raise e
+      end
+    end
+  
+  end
+
+  ::ActiveRecord::Base.send :include, ActiveRecord
+  
+end
